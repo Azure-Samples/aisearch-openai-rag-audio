465265805f7eb6a656f26a1990f58c209d27cfe1
LorenzoTullini <63406466+LorenzoTullini@users.noreply.github.com>
2024-10-04 09:57:52 +0200

    Fix start.sh
    Add echo message "open http://localhost:8765"

--- app/start.sh
+++ app/start.sh
@@ -32,7 +32,7 @@ fi
 echo ""
 echo "Starting backend"
 echo ""
-open http://127.0.0.1:8765
+echo "open http://127.0.0.1:8765"
 python app.py
 if [ $? -ne 0 ]; then
     echo "Failed to start backend"
