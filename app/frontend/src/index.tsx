13677d20bd8d36dbe440df8490ee69905c6c2d1d
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-10-18 11:21:07 +1100

    Start i18n translations

--- app/frontend/src/index.tsx
+++ app/frontend/src/index.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { I18nextProvider } from "react-i18next";
+import i18next from "./i18n/config";
+
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <I18nextProvider i18n={i18next}>
+            <App />
+        </I18nextProvider>
     </StrictMode>
 );
